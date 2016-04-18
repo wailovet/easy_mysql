18f95dab52321b1583f782d6e259049c41310125
wailovet <wailovet@163.com>
2016-04-18 16:29:27 +0800

    test

--- test/index.js
+++
@@ -1,9 +0,0 @@
-var easy_mysql = require('./lib/easy_mysql');
-easy_mysql.config({
-    "user": "travis"
-});
-
-
-
-
-var em = new easy_mysql("ox_admin");
---
+++ test/main.js
@@ -0,0 +1,15 @@
+var expect = require("expect.js")
+var easy_mysql = require('..');
+easy_mysql.config({
+    "user": "root"
+});
+em = new easy_mysql("test");
+
+it("add", function () {
+
+    em.add({"content":"testtest"},function(data){
+        if(data > 0){
+
+        }
+    });
+});
